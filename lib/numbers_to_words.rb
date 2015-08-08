b901afc679e09c8999eaa92b36ef2f5cf0dd4718
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-08-08 11:17:24 -0700

    Returns double digits.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,20 @@
 class Fixnum
   def numbers_to_words
-    num = {0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'}
-    return num.fetch(self)
+    base_num = {'0' => 'zero', '1' => 'one', '2' => 'two', '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six', '7' => 'seven',
+      '8' => 'eight', '9' => 'nine'}
+    tens = {'2' => 'twenty', '3' => 'thirty', '4' => 'forty', '5' => 'fifty', '6' => 'sixty', '7' => 'seventy', '8' => 'eighty',
+      '9' => 'ninety'}
+    numerals = self.to_s().reverse().split(//)
+    num_word = []
+    numerals.each_index do |i|
+      if i % 3 == 0
+        num_word.unshift(base_num.fetch(numerals[i]))
+      elsif
+        i % 3 == 1
+        num_word.unshift(tens.fetch(numerals[i]))
+      end
+    end
+    
+    return num_word.join(' ')
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,4 +5,8 @@ describe('Fixnum#numbers_to_words') do
   it('returns words for numbers 0-9') do
     expect(5.numbers_to_words()).to(eq('five'))
   end
+  
+  it('returns words for numbers 10-99') do
+    expect(24.numbers_to_words()).to(eq('twenty four'))
+  end
 end
